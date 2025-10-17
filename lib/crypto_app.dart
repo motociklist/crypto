be327c7aa6f89c4aad105254ed56d5b5c9dad3b8
frontend3 <frontend3@laboratoria.by>
2025-10-17 18:49:16 +0300

    [crypto-14] ~ refactor Talker

--- lib/crypto_app.dart
+++ lib/crypto_app.dart
@@ -1,6 +1,8 @@
 import 'package:crypto_app/router/router.dart';
 import 'package:crypto_app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,6 +13,11 @@ class MyApp extends StatelessWidget {
       title: 'Crypto',
       theme: dartTheme,
       routes: routes,
+      navigatorObservers: [
+        TalkerRouteObserver(
+          GetIt.I<Talker>()
+        ),
+      ],
     );
   }
 }
